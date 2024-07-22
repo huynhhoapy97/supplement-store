2a41c6710658efc2f1bba0fef078e240c5cd9216
Hoa Truong <huynhhoapy97@gmail.com>
2024-07-22 15:59:34 +0700

    Create Spring Boot project

---
+++ src/main/java/com/huynhhoapy97/SupplementStoreApplication.java
@@ -0,0 +1,11 @@
+package com.huynhhoapy97;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupplementStoreApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SupplementStoreApplication.class, args);
+    }
+}
